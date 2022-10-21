4d72eb95faa1f29142624f93050408c714971179
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-10-21 13:39:00 +0100

    fixed

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import SplashScreen from './src/screens/SplashScreen';
+ 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World, Welcome to Foodie!</Text>
+      <SplashScreen/>
       <StatusBar style="auto" />
     </View>
   );
