d9bd199bd5bd7bb2d77194a39eb3afd0da3d72c5
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-10-22 10:14:25 +0100

    Done with Splash Screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, useWindowDimensions } from 'react-native';
 import React from 'react';
+import Logo from '../../assets/img/FoodieLogoWhite.png'
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function SplashScreen(){
+export default function SplashScreen(navigator) {
+    const {height} = useWindowDimensions();
+
   return (
     <View>
-      <Text>SplashScreen</Text>
+      <LinearGradient
+        colors={["#B87B4E", "#EDAA78", "#B87B4E"]}
+        style={styles.background}
+        resizeMode = 'contain'
+      >
+        <Image
+            source={Logo}
+            style={[styles.logo, {height: height * 0.3}]}
+            resizeMode="contain"
+        />
+      </LinearGradient>
     </View>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    background: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    },
+    logo: {
+        flex: 1,
+        maxWidth: '100%',
+        maxHeight: '20%'
+    }
+});
